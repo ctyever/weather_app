c3418a17186cf22f527275d2cd617a070a77949d
ctyever <74819913+ctyever@users.noreply.github.com>
2021-07-18 23:13:38 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
-const API_KEY = 'a0fc372bb75934f6c6c7b3c82e6fc595'
+const API_KEY = ''
 
 export const getWeather = ( latitude, longitude ) => axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`)
